3bae2a5eb1d3e63afd0d69625834a61d6fead877
vinicius1263 <vinicius1263@hotmail.com>
2023-06-27 20:17:12 -0300

    up

---
+++ src/main/java/br/com/inicial/config/Scheuduler.java
@@ -0,0 +1,25 @@
+package br.com.inicial.config;
+
+
+import br.com.inicial.model.Pessoa;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableScheduling
+public class Scheuduler {
+
+
+    @Scheduled(initialDelay = 1, fixedDelay=60*1000 )
+    private void chamandoExecutar() {
+        try {
+            Pessoa pessoa = new Pessoa();
+            pessoa.falar();
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+
+}
