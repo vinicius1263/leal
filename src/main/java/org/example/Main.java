d2c411f17c46eb672e54185a23251afb0931a839
IlzaJava <ilzafernandescorreia@gmail.com>
2023-06-27 17:15:44 -0300

    alterei

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("teste 01");
+        System.out.println("hoje o dia foi kkkk");
 
     }
 }
