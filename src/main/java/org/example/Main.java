250b6992882836826895c86016c053a40f0c35c9
vinicius1263 <vinicius1263@hotmail.com>
2023-06-27 17:13:11 -0300

    teste 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Bem vindo ao java");
-        System.out.printf("a bola esta com vc");
+        System.out.println("teste 01");
 
     }
 }
